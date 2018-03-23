cd2db525d29bb65552bc3f6ad0cab5e75ce79abd
junw3i <cj@Juns-MacBook.local>
2018-03-23 17:19:57 +0800

    final solution

---
+++ controllers/users.js
@@ -0,0 +1,52 @@
+const jsonfile = require('jsonfile')
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  create: (req, res) => {
+    res.render('signup');
+  },
+
+  createUser: (req, res) => {
+    console.log(req.body);
+
+    jsonfile.readFile("users.json", (err, obj) => {
+      if (err) console.error(err);
+
+      bcrypt.hash(req.body.password, 1, (err, hash) => {
+        obj[req.body.name] = hash;
+
+        jsonfile.writeFile("users.json", obj, {spaces: 2}, (err2) => {
+          console.log(err2);
+        });
+        res.cookie("loggedin", true);
+        res.redirect("/");
+      });
+    });
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("loggedin");
+    res.redirect("/");
+  },
+
+  login: (req, res) => {
+    res.render('login');
+  },
+
+  checkLogin: (req, res) => {
+    let name = req.body.name;
+    let password = req.body.password;
+    // console.log(password);
+    bcrypt.hash(password, 1, (err, hash) => {
+      bcrypt.compare(password, hash, (err, results) => {
+        console.log(results);
+        if (results) {
+          res.cookie("loggedin", true);
+          res.redirect("/");
+        } else {
+          res.render("login");
+        }
+      });
+    });
+  }
+}
