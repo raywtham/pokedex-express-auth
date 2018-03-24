58bef2e70f9457eea3528bf6db4a6d33d693ec30
Alethea Toh <alethea.toh@duke.edu>
2018-03-24 16:00:56 +0800

    done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars');
 const jsonfile = require('jsonfile');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
 
 const FILE = 'pokedex.json';
 
@@ -22,6 +24,8 @@ app.set('view engine', 'handlebars');
 // Set static folder
 app.use(express.static('public'));
 
+app.use(cookieParser());
+
 // Set up body-parser to automatically parse form data into object
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -33,6 +37,112 @@ app.use(methodOverride('_method'));
  * Routes
  * ===================================
  */
+
+`ADD A NEW USER`
+
+app.get('/users/new', (request, response) => {
+  response.clearCookie('logged-in');
+  response.render('register');
+});
+
+app.post('/users', (request, response) => {
+  jsonfile.readFile('users.json', (err, obj) => {
+    if (err) console.error(err);
+
+    var name = request.body.name;
+    var password = request.body.password;
+
+    // hash the password
+    bcrypt.hash(password, 10, (err, hash) => {
+
+      // create new user object
+      let newUser = {
+        name: name,
+        hash: hash
+      }
+
+      // add to users.json
+      if (obj.users === undefined) obj.users = [];
+      obj.users.push(newUser);
+
+      jsonfile.writeFile('users.json', obj, {spaces: 4}, function(err2) {
+        if (err2) console.error(err2);
+
+        // Save a cookie to record that the user is currently logged in
+        response.cookie('logged-in', 'true');
+        // then redirect to the root '/' endpoint
+        response.redirect(301,'/');
+      });
+    });
+  });
+});
+
+'USER LOG IN'
+
+// Expose a new endpoint that intercepts GET requests to /users/logout
+app.get('/users/logout', (request, response) => {
+  console.log('logging out');
+  // clears the cookie's loggedin status
+  response.clearCookie('logged-in');
+  // redirects to '/'
+  response.redirect(301,'/');
+});
+
+// Expose a new endpoint that intercepts GET requests to /users/login, which responds with a HTML page with a user login form that has these fields: name and password
+app.get('/users/login', (request, response) => {
+  response.clearCookie('logged-in');
+  response.render('login');
+});
+
+// Point the form to submit data to the route (/users/login) using POST method
+app.post('/users/login', (request, response) => {
+  // Use bcrypt to compare the password in the form with the password saved in the JSON file. Save the loggedin status in the cookie and redirect to '/' if successful
+
+  var name = request.body.name;
+  var password = request.body.password; // plain text
+
+  // find name
+  jsonfile.readFile('users.json', (err, obj) => {
+    if (err) console.error(err);
+    var notFound = true;
+
+    var currUser;
+    for (var i=0;i<obj.users.length;i++) {
+      var user = obj.users[i];
+      // user found
+      if (user.name === name) {
+        currUser = user;
+      }
+    }
+
+    if (currUser) {
+      // retrive hash
+      var hash = currUser.hash;
+      // check if password is correct
+      bcrypt.compare(password, hash, function(err, res) {
+        console.log(hash);
+        if( res === true ){
+          // set cookie
+          response.cookie('logged-in', 'true');
+          response.redirect(301,'/');
+          return;
+        }
+        else {
+          console.log('incorrect password');
+          response.send("password incorrect!");
+          return;
+        }
+      });
+    }
+    else if (currUser === undefined) {
+      // if name not found - redirect to /users/new
+      console.log('user not found');
+      response.redirect('/users/new');
+      return;
+    }
+  });
+});
+
 app.get('/new', (request, response) => {
   // send response with some data
   response.render('new');
@@ -86,24 +196,40 @@ app.get('/:id', (request, response) => {
   });
 });
 
+`HOMEPAGE`
 app.get('/', (request, response) => {
+  console.log('im here - load homepage');
   jsonfile.readFile(FILE, (err, obj) => {
     if (err) console.error(err);
-    response.render('home', { pokemon: obj.pokemon });
+
+    // retrived logged-in cookie if it exists
+    var logged_in_cookie = request.cookies['logged-in'];
+    var logged_in;
+    if (logged_in_cookie !== undefined) logged_in = true;
+    else logged_in = false;
+    var context = {
+      pokemon: obj.pokemon,
+      logged_in: logged_in
+    }
+    response.render('home', context);
   });
 });
 
+`ADD A NEW POKEMON`
 app.post('/', (request, response) => {
+  console.log('im here - add a new pokemon');
   jsonfile.readFile(FILE, (err, obj) => {
     if (err) console.error(err);
 
     let newPokemon = request.body;
+    newPokemon.id = parseInt(newPokemon.id); // convert to integer
     obj.pokemon.push(newPokemon);
 
-    jsonfile.writeFile(FILE, obj, err2 => {
+    jsonfile.writeFile(FILE, obj, {spaces: 4}, function(err2) {
       if (err2) console.error(err2);
       response.render('home', { pokemon: obj.pokemon });
     });
+
   });
 });
 
@@ -127,10 +253,8 @@ app.put('/:id', (request, response) => {
       }
     }
     // save pokedex object in pokedex.json file
-    jsonfile.writeFile(FILE, obj, err2 => {
+    jsonfile.writeFile(FILE, obj, {spaces: 4}, function(err2) {
       if (err2) console.error(err2);
-
-      // redirect to GET /:id
       response.redirect(`/${request.params.id}`);
     });
   });
@@ -152,7 +276,7 @@ app.delete('/:id', (request, response) => {
       }
     }
     // save pokedex object in pokedex.json file
-    jsonfile.writeFile(FILE, obj, err2 => {
+    jsonfile.writeFile(FILE, obj, {spaces: 4}, err2 => {
       if (err2) console.error(err2);
 
       // redirect to GET /:id
